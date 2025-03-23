2a5a982da08ccb14a7fda087b705cac7f937887b
TAVeitchAU <163482132+TAVeitchAU@users.noreply.github.com>
2025-03-24 08:37:46 +1000

    Add files via upload

--- src/azure_client.py
+++ src/azure_client.py
@@ -1,18 +1,17 @@
+from azure.ai.inference import ChatCompletionsClient
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.inference.models import SystemMessage, UserMessage
+
 class AzureClient:
 
     def __init__(self, endpoint, api_key, model_name):
-        from azure.ai.inference import ChatCompletionsClient
-        from azure.core.credentials import AzureKeyCredential
-        
         self.client = ChatCompletionsClient(
-            endpoint="https://ai-tomveitch3489ai260312779439.openai.azure.com/openai/deployments/gpt-4o",
-            credential=AzureKeyCredential("1syRx28AXs1jDIi1gsIVfRPxvTBwORU6E0qkfGg3hGq7m5bs3k6wJQQJ99AKACYeBjFXJ3w3AAAAACOGkvdp")
+            endpoint=endpoint,
+            credential=AzureKeyCredential(api_key)
         )
-        self.model_name = "gpt-4o"  
+        self.model_name = model_name  
 
     def get_response(self, user_message):
-        from azure.ai.inference.models import SystemMessage, UserMessage
-        
         response = self.client.complete(
             stream=True,
             messages=[
